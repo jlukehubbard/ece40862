d50d35500843f414a1c0b0c9e2646235e78b2088
James Hubbard <hubbar31@purdue.edu>
2022-09-22 16:52:49 -0400

    program3b

--- lab1/program3b.py
+++ lab1/program3b.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
-def main():
+import time
 
-    print("Done!\n")
+def main():
+    rand = time.time_ns() % 11
+    print(rand)
+    guess = -1
+    i = 0
+    while ((guess != rand) and (i < 3)):
+        guess = int(input("Enter your guess:"))
+        i += 1
+
+    print("You win!") if (guess == rand) else print("You lose!")
 
 if __name__ == '__main__':
     main()
