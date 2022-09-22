237a7c7acbad152bff9bf806cf14ac919039f42e
James Hubbard <hubbar31@purdue.edu>
2022-09-22 12:48:40 -0400

    program2

--- lab1/program2.py
+++ lab1/program2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 def main():
-
-    print("Done!\n")
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    print("a = ", a)
+    num = int(input("Enter number: "))
+    newlist = [i for i in a if i < num]
+    print("The new list is ", newlist)
 
 if __name__ == '__main__':
     main()
